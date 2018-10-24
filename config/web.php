f7ec29a2d3bf0996066c43fecd1f772955d0a403
Alexey Fominih <site@marya.ru>
2018-10-24 11:59:59 +0400

    activerecord api, add vue

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'X7UBjVeW2wXyz_5o2mVaDWq0UA-uVm-I',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +46,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => false,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'department'],
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
---
+++ models/Employee.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "employee".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $department_id
+ */
+class Employee extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'employee';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'department_id'], 'required'],
+            [['department_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'department_id' => 'Department ID',
+        ];
+    }
+}
